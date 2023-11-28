12f24094be87f840f9ed0b14cf3a314323f822ad
Eric Saboia <saboia.eric@gmail.com>
2023-11-28 16:00:31 -0300

    refactor: add fake result

---
+++ Warehouse/Domain/Entity/Products.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entity
+{
+    public class Products
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = default;
+        public string  Description { get; set; } = default;
+        public short Amount { get; set; }
+    }
+}
---
+++ Warehouse/WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Bogus;
+using Domain.Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            _logger.LogInformation($"{nameof(ProductsController)} - {GetType().Name}:");
+
+            var faker = new Faker<Products>("pt_BR");
+
+            var products = faker.Generate(10);
+
+            return Ok(products);
+        }
+    }
+}
